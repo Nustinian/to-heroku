593d6fc9a3e7a2e46176f62722001acdeec11fba
Austin <50373319+Nustinian@users.noreply.github.com>
2020-02-04 00:28:01 +0900

    Revert "finally got it working... that was an annoying couple of hours! feels good now thoughgit add . (apparently psycopg is logging in as a remote host, not as a local user... makes sense..."
    This reverts commit c410eb2e1e146f7876b08d9cbd2618bd6e00d60f.

--- run.py
+++ run.py
@@ -1,12 +1,8 @@
 from app import app
 from db import db
-import os
 
 db.init_app(app)
 
 @app.before_first_request
 def create_tables():
-    try:
-      db.create_all()
-    except:
-      print(os.environ.get("DATABASE_URL"))
+    db.create_all()
